212f0c8af122308eec8da5a3566ea9a4b879b13b
piratefsh <schong7@wisc.edu>
2014-01-26 14:37:28 +0800

    Add toggle logo option

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
+//Event details animated hide/show
 $(function(){
-
 	$(".ce-default-hidden").hide();
-
 	$(".ce-btn-show-details").click(function(e){
 		e.preventDefault();
 		var eventDetailsObj = $(this).parents(".ce-event").children(".ce-event-details");
@@ -19,3 +18,34 @@ $(function(){
 		}
 	})
 });
+
+
+//Smooth scrolling. Taken from http://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
+//Toggle logo
+$(function(){
+	var dir = "img/"
+	var options = ["logo", "logo_alt", "logo_alt_2"];
+	var curr = 0;
+
+	$('.ce-btn-logo-toggle').click(function(){
+		curr = ++curr % options.length;
+		$('.navbar-brand > img').attr('src', dir + options[curr] + "_dark.png");
+		$('#about img.logo').attr('src', dir + options[curr] + ".png");
+	})
+
+})
